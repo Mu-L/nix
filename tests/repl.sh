277598f44969269f2056f8d51b8cd18bae3a8f71
regnat <rg@regnat.ovh>
2021-11-03 10:54:17 +0100

    Fix `nix repl`’s building of CA derivations
    When running a `:b` command in the repl, after building the derivations
    query the store for its outputs rather than just assuming that they are
    known in the derivation itself (which isn’t true for CA derivations)
    
    Fix #5328

--- tests/repl.sh
+++ tests/repl.sh
@@ -16,3 +16,5 @@ testRepl () {
 testRepl
 # Same thing (kind-of), but with a remote store.
 testRepl --store "$TEST_ROOT/store?real=$NIX_STORE_DIR"
+# Same thing, but with CA derivations
+NIX_TESTS_CA_BY_DEFAULT=1 testRepl
